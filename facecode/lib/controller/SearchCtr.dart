48487d6f10a9eedb3b6c1c5bc54e83679beed1dd
ShadyHammam02 <shadymhammam@yahoo.com>
2024-05-20 22:03:37 +0300

    SearchCtr

---
+++ facecode/lib/controller/SearchCtr.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facecode/model/entities/Post.dart';
+
+class Search{
+
+  void searchUsers(String firstName, String lastName) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+      .collection('Users')
+      .where('firstName', isEqualTo: firstName)
+      .where('lastName', isEqualTo: lastName)
+      .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      querySnapshot.docs.forEach((doc) {
+        var userData = doc.data();
+        print('User found: $userData');
+      });
+    }
+    else {
+      print('User not found');
+    }
+}
+
+
+  void searchPosts(String searchTerm) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('posts')
+        .where('textContent', isEqualTo: searchTerm)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      querySnapshot.docs.forEach((doc) {
+        var postData = doc.data();
+        print('Post found: $postData');
+      });
+    } 
+    else {
+      print('Post not found');
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////lesa hn3ml el groups////////////////////////////////////////////
+/*  
+  void searchGroups(String searchTerm) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('groups')
+        .where('name', isEqualTo: searchTerm)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      querySnapshot.docs.forEach((doc) {
+        var groupData = doc.data();
+        print('Group found: $groupData');
+      });
+    } 
+    else {
+      print('Group not found');
+    }
+  }
+*/
+}
+
+
